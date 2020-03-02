468605e4d31d647435e64937e56a4043ef004578
Tomasz Targoński <tomasz.targonski95@gmail.com>
2020-03-02 21:56:58 +0100

    Updates

---
+++ precache-manifest.1d80f2ab15e1ea97d77dc71c1c11a874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d2ecdb113ebb63540097dc757dddfa",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "29ea9569a0529f31470a",
+    "url": "/note-app/static/css/main.28f3bcd7.chunk.css"
+  },
+  {
+    "revision": "5c32fd5cd5c17bb4c2e1",
+    "url": "/note-app/static/js/2.adedea2d.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/note-app/static/js/2.adedea2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ea9569a0529f31470a",
+    "url": "/note-app/static/js/main.f3e2fa27.chunk.js"
+  },
+  {
+    "revision": "a42cee91c948babf8a8d",
+    "url": "/note-app/static/js/runtime-main.2b2f787f.js"
+  }
+]);
